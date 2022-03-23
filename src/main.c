196a19182d6325c0cfdc3e9d58d4e030832d482b
Richard <68392733+richard-on@users.noreply.github.com>
2022-03-23 23:28:41 +0000

    Add function to print symbol codes

--- src/main.c
+++ src/main.c
@@ -1,9 +1,23 @@
 /* main.c */
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-	printf("Hello World\n");
-	printf("Goodbye World\n");
+#define MAXLEN 300
+
+void printSymbolCodes(char* str) {
+  for(int i = 0; i < strlen(str); i++){
+    printf("%c%d", str[i], str[i]);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  if(argc != 2){
+    printf("incorrect number of arguments");
+    return 1;
+  }
+
+  char* str = argv[1];
+  printSymbolCodes(str);
 
 	return 0;
 }
